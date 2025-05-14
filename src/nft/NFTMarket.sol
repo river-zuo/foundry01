e34c52d0de2dd08db709884d307c920d1a8a313a
river-zuo <riverleft@hotmail.com>
2025-05-14 15:09:27 +0800

    feat: NFTMarket添加返回上架列表方法

--- src/nft/NFTMarket.sol
+++ src/nft/NFTMarket.sol
@@ -9,18 +9,42 @@ contract NFTMarket is BaseERC20CallBack {
 
     mapping(uint256 => uint256) pricesOfNFT;
 
+    NftDetail[] onList;
+
+    // id为0表示移除
+    struct NftDetail {
+        uint256 id;
+        uint256 price;
+        address owner;
+    }
+
     MyNFT public immutable _nft;
     BaseERC20 public immutable _erc20;
 
-    event NftOnList(uint256 indexed tokenId, address indexed seller, uint256 price);
+    event NftOnList(uint256  tokenId, address  seller, uint256 price);
 
-    event Transfer_NFT(address indexed from, address indexed to, uint256 tokenId);
+    event Transfer_NFT(address  from, address  to, uint256 tokenId);
 
-    event Refund_Excess_Amount(address indexed from, address indexed to, uint256 refund);
+    event Refund_Excess_Amount(address  from, address  to, uint256 refund);
+
+    event NFT_Received(address  from, address  to, uint256 tokenId);
+
+    event XHasChange(uint256 indexed i);
 
     constructor(address nftAddr, address erc20Addr) {
         _nft = MyNFT(nftAddr);
         _erc20 = BaseERC20(erc20Addr);
+        emit XHasChange(1);
+    }
+
+    // 获取上架列表
+    function nftList() public view returns (NftDetail[] memory) {
+        return onList;
+    }
+
+    // nft价格
+    function nftPrice(uint256 tokenId) public view returns (uint256) {
+        return pricesOfNFT[tokenId];
     }
 
     // NFT的持有者上架NFT, 两种实现方式: 1.授权 2.转到该合约
@@ -34,6 +58,13 @@ contract NFTMarket is BaseERC20CallBack {
         require((approveAddr == address(this)), "NFTMarket has not get approved");
         // 上架
         pricesOfNFT[tokenId] = amount;
+        
+        NftDetail memory detail = NftDetail({
+            id: tokenId,
+            price: amount,
+            owner: owner
+        });
+        onList.push(detail);
         emit NftOnList(tokenId, msg.sender, amount);
     }
 
@@ -49,6 +80,8 @@ contract NFTMarket is BaseERC20CallBack {
         require(success, "thansfer success");
         _nft.transferFrom(old_holder, nft_new_holder, tokenId);
         pricesOfNFT[tokenId] = 0;
+        // 移除列表
+        _deleteNFTonList(tokenId);
         emit Transfer_NFT(old_holder, nft_new_holder, tokenId);
     }
 
@@ -67,7 +100,21 @@ contract NFTMarket is BaseERC20CallBack {
         address old_holder = _nft.ownerOf(tokenId);
         address nft_new_holder = account;
         _nft.transferFrom(old_holder, nft_new_holder, tokenId);
+        emit NFT_Received(old_holder, nft_new_holder, tokenId);
+        // 移除列表
+        _deleteNFTonList(tokenId);
         return BaseERC20CallBack.tokensReceived.selector;
+    }
+
+    function _deleteNFTonList(uint256 tokenId) internal  {
+        for (uint256 i=0; i<onList.length;i++) {
+            NftDetail memory detail = onList[i];
+            if (detail.id == tokenId) {
+                detail.id = 0;
+                onList[i] = detail;
+                break ;
+            }
+        }
     }
 
 }
